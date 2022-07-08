dc680c2062b1696d2045b5933991e4298cd1b6c2
sudac_tudak <30slava09@mail.ru>
2022-07-08 20:05:24 +0300

    Add Yandex-metrika

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -1,19 +1,35 @@
 import { AppProps } from 'next/app';
 import Head from 'next/head';
+import ym, { YMInitializer } from 'react-yandex-metrika';
 import '../styles/globals.scss';
 
 function MyApp({ Component, pageProps, router }: AppProps): JSX.Element {
+  router.events.on('routeChangeComplete', (url: string) => {
+    if (typeof window !== 'undefined') {
+      ym('hit', url);
+    }
+  });
+
   return (
     <>
       <Head>
         <title>My OWL Top</title>
         <link rel="icon" href="/favicon.ico" />
-        <meta httpEquiv="X-UA-Compatible" content="IE=edge"/>
-        <meta name="viewport" content="width=device-width, initial-scale=1.0"/>
-        <meta name="theme-color" content="#111111"></meta>
-        <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath}/>
-        <meta property="og:locale" content="ru_RU"/>
+        <link rel="preconnect" href="https://mc.yandex.ru" />
+        <meta httpEquiv="X-UA-Compatible" content="IE=edge" />
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+        <meta name="theme-color" content="#111111" />
+        <meta property="og:url" content={process.env.NEXT_PUBLIC_DOMAIN + router.asPath} />
+        <meta property="og:locale" content="ru_RU" />
       </Head>
+      <YMInitializer
+        accounts={[]}
+        options={{
+          webvisor: true,
+          defer: true,
+        }}
+        version="2"
+      />
       <Component {...pageProps} />
     </>
   );
